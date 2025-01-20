e545fc9d49b7674f906d6901f920e0a368f88da5
Memet Zx <zxce3@gnuweeb.org>
2025-01-21 01:31:55 +0700

    Move install.php to pages directory and refactor display logic

--- src/pages/install.php
+++ src/pages/install.php
@@ -1,18 +1,15 @@
 <?php
+
+function page_install(array &$opt)
+{
+    $opt['title'] = 'Install Pocketbase';
+    $opt['content'] = displayInstall();
+}
+
 function displayInstall()
 {
+
     ?>
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pocketbase Installation</title>
-        <?php getCssStyle(); ?>
-    </head>
-
-    <body>
         <h1>Install Pocketbase</h1>
         <form method="POST">
             <label for="email">Email:</label>
@@ -23,9 +20,6 @@ function displayInstall()
             <br>
             <button type="submit">Install</button>
         </form>
-    </body>
-
-    </html>
     <?php
 }
 ?>
